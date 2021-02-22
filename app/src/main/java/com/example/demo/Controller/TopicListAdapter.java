b7236eb627bf50f1cc9a3dbeb3eef9547e2e073b
wangida02 <wangida02@gmail.com>
2021-02-22 02:11:07 -0500

    icons for topic list items

--- app/src/main/java/com/example/demo/Controller/TopicListAdapter.java
+++ app/src/main/java/com/example/demo/Controller/TopicListAdapter.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -19,22 +20,30 @@ public class TopicListAdapter extends RecyclerView.Adapter<TopicListAdapter.View
     }
 
     private String[] topics;
+    private int[] icons;
     private ItemClickListener listener;
 
-    TopicListAdapter(Context context, String[] topics) {
+    TopicListAdapter(Context context, String[] topics, int[] icons) {
         this.topics = topics;
+        this.icons = icons;
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         private TextView textView;
+        private ImageView icon;
         public ViewHolder(View view) {
             super(view);
             view.setOnClickListener(this);
             textView = (TextView) view.findViewById(R.id.topic_list_item_text);
+            icon = (ImageView) view.findViewById(R.id.topic_list_icon);
         }
 
         public TextView getTextView() {
             return textView;
+        }
+
+        public ImageView getIcon() {
+            return icon;
         }
 
         @Override
@@ -53,6 +62,7 @@ public class TopicListAdapter extends RecyclerView.Adapter<TopicListAdapter.View
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         holder.getTextView().setText(topics[position]);
+        holder.getIcon().setImageResource(icons[position]);
     }
 
     String getTopicName(int position) {
