21dfa10a1090005c846c5890318b5ce6a4f71946
Rakeshgore <42789046+Rakeshgore@users.noreply.github.com>
2021-03-12 17:26:37 -0500

    Added repeat button functionality in lessons

--- app/src/main/java/com/example/demo/Controller/Lesson.java
+++ app/src/main/java/com/example/demo/Controller/Lesson.java
@@ -24,6 +24,7 @@ public class Lesson extends AppCompatActivity {
     ImageButton quizButton;
     ImageButton repeatButton;
     String lessonNameString;
+    List<Integer> soundsList;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,9 +50,8 @@ public class Lesson extends AppCompatActivity {
             exampleDescription.setText(answer);
         }
         buttonVisibility();
-        List<Integer>  soundsList= lessonSounds(lessonNameString);
+        soundsList= lessonSounds(lessonNameString);
         new Handler().postDelayed(() -> playSound(soundsList), 1000);
-
     }
 
     public void playSound(List<Integer> soundsList){
@@ -73,78 +73,105 @@ public class Lesson extends AppCompatActivity {
     public List<Integer> lessonSounds(String lessonNameString){
         List<Integer> soundsList = new ArrayList<>();
         //Hard and Soft c & g
-        if(lessonNameString.equals("c")) {
-            soundsList = Arrays.asList(R.raw.cat,R.raw.canary,R.raw.cake);
-        } // Still Needed: c2,g,g1,g2
-        //Beginning 3 Letter Blends
-        else if(lessonNameString.equals("scr")) {
-            soundsList = Arrays.asList(R.raw.scrub,R.raw.scratch,R.raw.scream);
-        }else if(lessonNameString.equals("spl")) {
-            soundsList = Arrays.asList(R.raw.split,R.raw.splat,R.raw.splash);
-        }else if(lessonNameString.equals("spr")) {
-            soundsList = Arrays.asList(R.raw.spread,R.raw.spruce,R.raw.sprain);
-        }else if(lessonNameString.equals("str")) {
-            soundsList = Arrays.asList(R.raw.strong,R.raw.straw,R.raw.string);
-        }
-        //Silent
-        else if(lessonNameString.equals("gh")) {
-            soundsList = Arrays.asList(R.raw.high,R.raw.right,R.raw.eight);
-        }else if(lessonNameString.equals("kn")) {
-            soundsList = Arrays.asList(R.raw.knot,R.raw.knee,R.raw.knit);
-        }else if(lessonNameString.equals("sc")) {
-            soundsList = Arrays.asList(R.raw.scent,R.raw.scene,R.raw.science);
-        }else if(lessonNameString.equals("wr")) {
-            soundsList = Arrays.asList(R.raw.wrap,R.raw.write,R.raw.wrong);
-        }
-        //Pairs
-        else if(lessonNameString.equals("ai")) {
-            soundsList = Arrays.asList(R.raw.tail,R.raw.nail,R.raw.rainbow);
-        }else if(lessonNameString.equals("au")) {
-            soundsList = Arrays.asList(R.raw.haul,R.raw.gauze,R.raw.faucet);
-        }else if(lessonNameString.equals("aw")) {
-            soundsList = Arrays.asList(R.raw.saw,R.raw.hawk,R.raw.paws);
-        }else if(lessonNameString.equals("ay")) {
-            soundsList = Arrays.asList(R.raw.jay,R.raw.ray,R.raw.play);
-        }else if(lessonNameString.equals("ee")) {
-            soundsList = Arrays.asList(R.raw.bee,R.raw.tree,R.raw.jeep);
-        }else if(lessonNameString.equals("ew")) {
-            soundsList = Arrays.asList(R.raw.flew,R.raw.blew,R.raw.chew);
-        }else if(lessonNameString.equals("oa")) {
-            soundsList = Arrays.asList(R.raw.goat,R.raw.boat,R.raw.coat);
-        }else if(lessonNameString.equals("oi")) {
-            soundsList = Arrays.asList(R.raw.coin,R.raw.point,R.raw.choice);
-        }else if(lessonNameString.equals("oo")) {
-            soundsList = Arrays.asList(R.raw.zoo,R.raw.moon); // Add Hoof
-        }else if(lessonNameString.equals("ou")) {
-            soundsList = Arrays.asList(R.raw.house,R.raw.mouth, R.raw.proud);
-        }else if(lessonNameString.equals("ow")) {
-            soundsList = Arrays.asList(R.raw.blow,R.raw.snow, R.raw.throw1);
-        }else if(lessonNameString.equals("oy")) {
-            soundsList = Arrays.asList(R.raw.boy,R.raw.toys, R.raw.oyster);
-        }//Still Needed: ea,ea1,oo1,ow1
-        //With R
-        else if(lessonNameString.equals("ar")) {
-            soundsList = Arrays.asList(R.raw.arm,R.raw.stars, R.raw.barn);
-        }else if(lessonNameString.equals("er")) {
-            soundsList = Arrays.asList(R.raw.fern,R.raw.zipper);// ADD Tiger
-        }else if(lessonNameString.equals("ir")) {
-            soundsList = Arrays.asList(R.raw.bird,R.raw.girl); // ADD Shirt
-        }else if(lessonNameString.equals("or")) {
-            soundsList = Arrays.asList(R.raw.corn,R.raw.orca); //ADD Horns
-        }else if(lessonNameString.equals("ur")) {
-            soundsList = Arrays.asList(R.raw.burn); //ADD Surf/Nurse
-        }//Still Needed: ar1, or1
-        //SCHWAS
-        else if(lessonNameString.equals("a")) {
-            soundsList = Arrays.asList(R.raw.sofa,R.raw.zebra, R.raw.afraid);
-        }else if(lessonNameString.equals("e")) {
-            soundsList = Arrays.asList(R.raw.camel,R.raw.oven, R.raw.kitten);
-        }else if(lessonNameString.equals("i")) {
-            soundsList = Arrays.asList(R.raw.rabbit,R.raw.pencil, R.raw.robin);
-        }else if(lessonNameString.equals("o")) {
-            soundsList = Arrays.asList(R.raw.lion,R.raw.oven,R.raw.wagon);
-        } else if(lessonNameString.equals("u")) {
-            soundsList = Arrays.asList(R.raw.bug,R.raw.bus, R.raw.cut);
+        switch (lessonNameString) {
+            case "c":
+                soundsList = Arrays.asList(R.raw.cat, R.raw.canary, R.raw.cake);
+                break;
+            //Beginning 3 Letter Blends
+            case "scr":
+                soundsList = Arrays.asList(R.raw.scrub, R.raw.scratch, R.raw.scream);
+                break;
+            case "spl":
+                soundsList = Arrays.asList(R.raw.split, R.raw.splat, R.raw.splash);
+                break;
+            case "spr":
+                soundsList = Arrays.asList(R.raw.spread, R.raw.spruce, R.raw.sprain);
+                break;
+            case "str":
+                soundsList = Arrays.asList(R.raw.strong, R.raw.straw, R.raw.string);
+                break;
+            //Silent
+            case "gh":
+                soundsList = Arrays.asList(R.raw.high, R.raw.right, R.raw.eight);
+                break;
+            case "kn":
+                soundsList = Arrays.asList(R.raw.knot, R.raw.knee, R.raw.knit);
+                break;
+            case "sc":
+                soundsList = Arrays.asList(R.raw.scent, R.raw.scene, R.raw.science);
+                break;
+            case "wr":
+                soundsList = Arrays.asList(R.raw.wrap, R.raw.write, R.raw.wrong);
+                break;
+            //Pairs
+            case "ai":
+                soundsList = Arrays.asList(R.raw.tail, R.raw.nail, R.raw.rainbow);
+                break;
+            case "au":
+                soundsList = Arrays.asList(R.raw.haul, R.raw.gauze, R.raw.faucet);
+                break;
+            case "aw":
+                soundsList = Arrays.asList(R.raw.saw, R.raw.hawk, R.raw.paws);
+                break;
+            case "ay":
+                soundsList = Arrays.asList(R.raw.jay, R.raw.ray, R.raw.play);
+                break;
+            case "ee":
+                soundsList = Arrays.asList(R.raw.bee, R.raw.tree, R.raw.jeep);
+                break;
+            case "ew":
+                soundsList = Arrays.asList(R.raw.flew, R.raw.blew, R.raw.chew);
+                break;
+            case "oa":
+                soundsList = Arrays.asList(R.raw.goat, R.raw.boat, R.raw.coat);
+                break;
+            case "oi":
+                soundsList = Arrays.asList(R.raw.coin, R.raw.point, R.raw.choice);
+                break;
+            case "oo":
+                soundsList = Arrays.asList(R.raw.zoo, R.raw.moon); // Add Hoof
+                break;
+            case "ou":
+                soundsList = Arrays.asList(R.raw.house, R.raw.mouth, R.raw.proud);
+                break;
+            case "ow":
+                soundsList = Arrays.asList(R.raw.blow, R.raw.snow, R.raw.throw1);
+                break;
+            case "oy":
+                soundsList = Arrays.asList(R.raw.boy, R.raw.toys, R.raw.oyster);
+                break;
+            //With R
+            case "ar":
+                soundsList = Arrays.asList(R.raw.arm, R.raw.stars, R.raw.barn);
+                break;
+            case "er":
+                soundsList = Arrays.asList(R.raw.fern, R.raw.zipper);// ADD Tiger
+                break;
+            case "ir":
+                soundsList = Arrays.asList(R.raw.bird, R.raw.girl); // ADD Shirt
+                break;
+            case "or":
+                soundsList = Arrays.asList(R.raw.corn, R.raw.orca); //ADD Horns
+                break;
+            case "ur":
+                soundsList = Arrays.asList(R.raw.burn); //ADD Surf/Nurse
+                break;
+            //SCHWAS
+            case "a":
+                soundsList = Arrays.asList(R.raw.sofa, R.raw.zebra, R.raw.afraid);
+                break;
+            case "e":
+                soundsList = Arrays.asList(R.raw.camel, R.raw.oven, R.raw.kitten);
+                break;
+            case "i":
+                soundsList = Arrays.asList(R.raw.rabbit, R.raw.pencil, R.raw.robin);
+                break;
+            case "o":
+                soundsList = Arrays.asList(R.raw.lion, R.raw.oven, R.raw.wagon);
+                break;
+            case "u":
+                soundsList = Arrays.asList(R.raw.bug, R.raw.bus, R.raw.cut);
+                break;
         }
         return soundsList;
     }
@@ -211,6 +238,10 @@ public class Lesson extends AppCompatActivity {
         quizButton.setVisibility(View.VISIBLE);
         repeatButton = findViewById(R.id.repeat_button);
         repeatButton.setVisibility(View.VISIBLE);
+    }
+
+    public void onRepeat(View view){
+        playSound(soundsList);
     }
 
     public void onQuiz(View view) {
