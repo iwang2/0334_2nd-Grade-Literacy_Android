54aa1fcc01cdc68f693a64a1c5de52313b4619e0
foreverjerry7 <zhangju0707@gmail.com>
2021-03-15 12:50:36 +0800

    changed mp3 and quiz lessonToAnswers

--- app/src/main/java/com/example/demo/Controller/CompletedPuzzle.java
+++ app/src/main/java/com/example/demo/Controller/CompletedPuzzle.java
@@ -31,7 +31,7 @@ public class CompletedPuzzle extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.completedpuzzle);
 
     }
 
